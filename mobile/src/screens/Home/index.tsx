3f4b521938198f780a37005e21d26a3fafa2a9a5
Felipe Amaral <felipeamaraldev15@gmail.com>
2022-09-16 22:55:00 -0300

    acessando dados da API com react native

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View , Image, FlatList} from 'react-native';
 
 import { styles } from './styles';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { Heading } from '../../components/Heading';
-import { GameCard } from '../../components/GameCard';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 import { GAMES } from '../../utils/games';
 
 
 export function Home() {
+  useEffect( () => {
+    fetch('http://192.168.1.107:3330/games')
+    .then(response => response.json())
+    .then(data => setGames(data))
+  }, [])
+
+  const [games, setGames] = useState<GameCardProps[]>([]);
   return (
     <View style={styles.container}>
       <Image 
@@ -23,7 +30,7 @@ export function Home() {
       />
 
       <FlatList 
-        data={GAMES}
+        data={games}
         keyExtractor={ item => item.id}
         renderItem={ ({item}) => (
           <GameCard 
